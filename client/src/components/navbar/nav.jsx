595d3ff531e2a678512b36bb1cb7eabb7a760ef1
MohamedBerbouchi <82718864+MohamedBerbouchi@users.noreply.github.com>
2024-05-19 00:02:26 +0100

    add menu and responsive

--- client/src/components/navbar/nav.jsx
+++ client/src/components/navbar/nav.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './nav.scss'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./nav.scss";
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
     <nav>
       <div className="left">
-          <Link to='/' className="logo link">
-            <img src="/logo.png" alt="" />
-            <span>MinaEstate</span>
-          </Link>
-          <Link to='/'  className='link'>
-            Home
-          </Link>
-          <Link to='/' className='link'>
-            About
-          </Link>
-          <Link to='/' className='link'>
-            Contact
-          </Link>
-          <Link to='/' className='link'>
-            Agents
-          </Link>
+        <Link to="/" className="logo link">
+          <img src="/logo.png" alt="" />
+          <span>MinaEstate</span>
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          About
+        </Link>
+        <Link to="/" className="link">
+          Contact
+        </Link>
+        <Link to="/" className="link">
+          Agents
+        </Link>
       </div>
       <div className="right">
-          <button>Sign in</button>
-          <button>Sign up</button>
+        <button>Sign in</button>
+        <button>Sign up</button>
+        <div className="menu" onClick={() => setOpen(!open)}>
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className={open ? "mobile_menu active" : "mobile_menu"}>
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="/" className="link">
+            About
+          </Link>
+          <Link to="/" className="link">
+            Contact
+          </Link>
+          <Link to="/" className="link">
+            Agents
+          </Link>
+          <Link to="/" className="link">
+            Sign in 
+          </Link>
+          <Link to="/" className="link">
+           Sign up
+          </Link>
+        </div>
+     
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
